468d5de049f472f938ca6442b26970a76a2f8ede
Brendan Whitfield <brendanw@windworksdesign.com>
2014-11-12 22:15:44 -0500

    started writing definition finder

--- server/src/parse.js
+++ server/src/parse.js
@@ -3,7 +3,15 @@ var fs        = require("fs");
 var preproc   = require("./preprocessor.js");
 var tokenizer = require("./tokenizer.js");
 
+
+
+
+
 var c = fs.readFileSync("./tests/hello/hello.c").toString("utf8");
+
+
+
+
 
 var tokens = tokenizer.run(preproc(c));
 
@@ -12,19 +20,20 @@ var tokens = tokenizer.run(preproc(c));
 
 
 
-//chunker (split tokens into statements by close_brackets and semicolons)
+//chunker (split tokens into statements by brackets and semicolons)
 
 var statements = [];
 var current = [];
 var parenLevel = 0; //prevents semicolons from chunk while in for(;;) loops
 
 //increments or decrements parenLevel
-function manageParens(t)
+function countParens(token)
 {
-	if(t.type === tokenizer.types.OPEN_PAREN)
-		parenLevel++;
-	else if(t.type === tokenizer.types.CLOSE_PAREN)
-		parenLevel--;
+	if(token.type === tokenizer.types.OPEN_PAREN)
+		return 1;
+	else if(token.type === tokenizer.types.CLOSE_PAREN)
+		return -1;
+	return 0;
 }
 
 function push()
@@ -38,7 +47,7 @@ for(var i = 0; i < tokens.length; i++)
 	var token = tokens[i];
 	current.push(token);
 
-	manageParens(token);
+	parenLevel += countParens(token);
 
 	//decide whether the current token string is a statement chunk
 	if(token.type === tokenizer.types.OPEN_BRACKET)
@@ -49,28 +58,60 @@ for(var i = 0; i < tokens.length; i++)
 		push();
 }
 
+
 //console.log(statements);
 
 
 
 
+//returns the index of the matching close paren in a statement
+function matchParen(statement, open)
+{
+	var level = 1;
+
+	//loop until the paren level returns to zero
+	for(var t = open+1; t < statement.length; t++)
+	{
+		var token = statement[t];
+		level += countParens(token);
+		if(level === 0) return t;
+	}
+}
 
 
-//interpretter (finds function definitions)
+//test for a function definition at the given token index
+function testFuncDef(statement, start)
+{
+	if(statement[t].type === tokenizer.types.IDENTIFIER) //function name
+	{
+		if(statement[t+1].type === tokenizer.types.OPEN_PAREN) //open paren
+		{
+			//find matching paren
+			var close = matchParen(statement, t+1);
+			if(close !== -1) //close paren
+			{
+				console.log("---------->",token);
+			}
+		}
+	}
+}
 
 
+
+//find function definitions
 for(var i = 0; i < statements.length; i++)
 {
 	var statement = statements[i];
-	
-	parenLevel = 0;
-
 	console.log("========");
 
-	for(var t = 0; t < statement.length; t++)
+	//minimum number of tokens needed to form function DEFINITION
+	// void main ( ) {
+	if(statement.length >= 5)
 	{
-		var token = statement[t];
-		manageParens(token);
-		console.log(parenLevel, token);
+		for(var t = 0; t < statement.length - 4; t++)
+		{
+			//test using this position as a starting point
+			var test = testFuncDef(statement, t);
+		}
 	}
 }
