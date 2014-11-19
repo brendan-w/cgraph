d72a81073fb608727f4e91396fef4e22020307d5
Will <whp3652@rit.edu>
2014-11-19 15:38:54 -0500

    initial frontend attempt

---
+++ frontend/multi-node.js
@@ -0,0 +1,162 @@
+var links = [{source: "get_mem_from_pool", target: "memmgr_free", type: "licensing"},
+             {source: "memmgr_alloc", target: "get_mem_from_pool", type: "suit"},
+             {source: "memmgr_init", target: "memmgr_free", type: "suit"},
+             {source: "memmgr_free", target: "memmgr_free", type: "resolved"}];
+
+var definitions = [ {
+    "filename": "../tests/memmgr/memmgr.c",
+    "functions": [
+      {
+        "name": "memmgr_init",
+        "public": true,
+        "line": 36,
+        "calls": []
+      },
+      {
+        "name": "get_mem_from_pool",
+        "public": false,
+        "line": 43,
+        "calls": [
+        {
+          "name": "memmgr_free",
+          "linked": true,
+          "file_ID": 0,
+          "func_ID": 3
+        }
+        ]
+      },
+      {
+        "name": "memmgr_alloc",
+        "public": true,
+        "line": 71,
+        "calls": [
+        {
+          "name": "get_mem_from_pool",
+          "linked": true,
+          "file_ID": 0,
+          "func_ID": 1
+        }
+        ]
+      },
+      {
+        "name": "memmgr_free",
+        "public": true,
+        "line": 134,
+        "calls": []
+      }
+    ]
+  },
+  {
+    "filename": "../tests/memmgr/memmgr.h",
+    "functions": []
+} ];
+
+defs = d3.map(definitions, function(d) { return d.functions; }).entries();
+console.log(defs);
+
+//sort links by source, then target
+links.sort(function(a,b) {
+    if (a.source > b.source) {return 1;}
+    else if (a.source < b.source) {return -1;}
+    else {
+        if (a.target > b.target) {return 1;}
+        if (a.target < b.target) {return -1;}
+        else {return 0;}
+    }
+});
+
+//any links with duplicate source and target get an incremented 'linknum'
+for (var i=0; i<links.length; i++) {
+    if (i !== 0 &&
+        links[i].source == links[i-1].source &&
+        links[i].target == links[i-1].target) {
+            links[i].linknum = links[i-1].linknum + 1;
+        }
+    else {
+      links[i].linknum = 1;
+    }
+}
+
+var nodes = {};
+
+// Compute the distinct nodes from the links.
+links.forEach(function(link) {
+  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+});
+
+var w = 600,
+    h = 600;
+
+var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([w, h])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick)
+    .start();
+
+var svg = d3.select("body").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+// Per-type markers, as they don't inherit styles.
+svg.append("svg:defs").selectAll("marker")
+    .data(["suit", "licensing", "resolved"])
+  .enter().append("svg:marker")
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+var path = svg.append("svg:g").selectAll("path")
+    .data(force.links())
+  .enter().append("svg:path")
+    .attr("class", function(d) { return "link " + d.type; })
+    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+var circle = svg.append("svg:g").selectAll("circle")
+    .data(force.nodes())
+  .enter().append("svg:circle")
+    .attr("r", 6)
+    .call(force.drag);
+
+var text = svg.append("svg:g").selectAll("g")
+    .data(force.nodes())
+  .enter().append("svg:g");
+
+// A copy of the text with a thick white stroke for legibility.
+text.append("svg:text")
+    .attr("x", 8)
+    .attr("y", ".31em")
+    .attr("class", "shadow")
+    .text(function(d) { return d.name; });
+
+text.append("svg:text")
+    .attr("x", 8)
+    .attr("y", ".31em")
+    .text(function(d) { return d.name; });
+
+// Use elliptical arc path segments to doubly-encode directionality.
+function tick() {
+  path.attr("d", function(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = 75/d.linknum;  //linknum is defined above
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  });
+
+  circle.attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  });
+
+  text.attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  });
+}
