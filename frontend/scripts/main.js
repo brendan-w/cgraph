a6ff4b68cb9e749109aec4fbb0e37edaa736f246
Will <whp3652@rit.edu>
2014-11-20 01:12:44 -0500

    Make the network not look so shitty

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,12 +1,11 @@
 var code_viewer = document.querySelector("#js_code_viewer"),
-    current_file = "data/SENNA_NER.c";
+    current_file = "../server/tests/memmgr/memmgr.c";
 
 reqwest(current_file, function(res) {
   code_viewer.innerHTML = res;
 
   // If we're using syntax highlighting, update it on the new file
   if (Prism) {
-    console.log('tol');
     Prism.highlightElement(code_viewer);
   }
 });
