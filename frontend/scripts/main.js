5504806c37da875f23e8384fbce9e24167e3de31
Will <whp3652@rit.edu>
2014-11-24 15:03:38 -0500

    Push node_modules as is to the server

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -3,6 +3,7 @@ var code_viewer = document.querySelector("#js_code_viewer"),
 
 reqwest(current_file, function(res) {
   code_viewer.innerHTML = res;
+  console.log(res);
 
   // If we're using syntax highlighting, update it on the new file
   if (Prism) {
