6083e073e172644089ecb77048aa00d7d29381ac
Brendan Whitfield <brendanw@windworksdesign.com>
2014-12-11 23:00:41 -0500

    added filename and HTML escaping

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,6 +1,22 @@
 
+var file_name   = document.querySelector("#file_name");
 var code_viewer = document.querySelector("#js_code_viewer");
-var currentFile = null;
+var header      = document.querySelector("header");
+var viz_column  = document.querySelector(".viz_column");
+var code_column = document.querySelector(".code_column");
+var col_height  = window.innerHeight - header.clientHeight;
+
+code_column.style.height = col_height;
+viz_column.style.height  = col_height;
+viz_column.setAttribute("height", col_height);
+
+
+function escapeHTML(str)
+{
+	var div = document.createElement('div');
+	div.appendChild(document.createTextNode(str));
+	return div.innerHTML;
+}
 
 
 function goto_line(line)
@@ -17,17 +33,21 @@ function goto_line(line)
 	//console.log(line);
 	elem = document.querySelector(".line-numbers-rows :nth-child(" + line + ")");
 	elem.scrollIntoView();
+
+	//scrolls all the way up, includes the padding at the top
+	if(!line)
+		code_viewer.parentNode.scrollTop = 0;
 }
 
 function goto_code(filename, line)
 {
-	if(filename !== currentFile)
+	if(file_name.innerHTML !== filename)
 	{
 		var url = "/file?user=" + user + "&repo=" + repo + "&filename=" + filename;
 		reqwest(url, function(body) {
-			code_viewer.innerHTML = body;
+			code_viewer.innerHTML = escapeHTML(body);
 			goto_line(line);
-			currentFile = filename;
+			file_name.innerHTML = filename;
 		});
 	}
 	else
@@ -35,13 +55,3 @@ function goto_code(filename, line)
 		goto_line(line);
 	}
 }
-
-
-var header      = document.querySelector("header");
-var viz_column  = document.querySelector(".viz_column");
-var code_column = document.querySelector(".code_column");
-var col_height  = window.innerHeight - header.clientHeight;
-
-code_column.style.height = col_height;
-viz_column.style.height = col_height;
-viz_column.setAttribute("height", col_height);
